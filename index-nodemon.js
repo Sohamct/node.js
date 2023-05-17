ed41581e24845015e0f3f033194355bcb21bcefc
Soham@git <sohamtejani699@gmail.com>
2023-05-17 19:47:18 +0530

    init

---
+++ index-nodemon.js
@@ -0,0 +1,14 @@
+//4
+// do npm i nodemon -g for installing nodemon globally.
+
+console.warn("Try Nodemon code s by s");
+// node.js is async.
+//Sync: if 4 scripts then it will run scripts in the order and 
+// if any scripts takes more than specified minute it will execute it whether 
+// whenever it is stopped.
+
+//aSync: if 4 scripts then it will run scripts in the order and 
+// if any scripts takes more than  specified minute it will stop executing that script and 
+// will excute the next script.
+//eg: node.js, java 
+//They are superfast.
